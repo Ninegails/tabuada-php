7b9b79753aadc15c61e2329a17903ee8e544fd57
Guilherme Pires <39858141+Ninegails@users.noreply.github.com>
2019-09-25 12:19:48 -0300

    Update tabuada.php

--- tabuada.php
+++ tabuada.php
@@ -1,20 +1,15 @@
 <?php
 
-
-$tabuada = 5;
-
-while($tabuada < 11){
-
-$vezes = 0;
-
-for($numero = $vezes; $numero < 11; $numero++) {
-    echo "$tabuada x $numero=";
-    echo $tabuada*$numero;
-    echo "<br>";
-    $numero++ ;
-
+$tabuada = 0;
+while ($tabuada <= 10)
+{
+echo "<br>Tabuada do $tabuada <br>";
+$multiplicador = 0;
+while($multiplicador<=10)
+{
+$result = $tabuada * $multiplicador;
+echo "$tabuada x $multiplicador = $result<br>";
+$multiplicador++;
 }
-
-$tabuada++;
-
+ $tabuada++;
 }
